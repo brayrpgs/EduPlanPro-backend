a388817c4bb2c7866170ca013f5072e102e89e60
brayRPGs <brayanymelquisedec@gmail.com>
2025-03-11 18:01:04 -0600

    brayan rosales perez     11/03/2025 16:00 CR     fix de un console log el modelo de propgramas del curso

--- data/CourseProgram.js
+++ data/CourseProgram.js
@@ -39,7 +39,7 @@ class CourseProgram {
             await stmt.query(sql, values);
             return true;
         } catch (error) {
-            console.log(error);
+            //console.log(error);
             return error.code;
         }
         finally {
