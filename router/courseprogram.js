66ff7352d8dba4740a7aefb2d8a936bba06028e3
brayRPGs <brayanymelquisedec@gmail.com>
2025-03-13 18:45:36 -0600

    brayan rosales perez --se implementa la funcionalidad de eliminar programas del curso -- se finaliza el modulo de administrar programas del curso --numero de tarea (20) --finaliza la HU numero 15

--- router/courseprogram.js
+++ router/courseprogram.js
@@ -46,7 +46,7 @@ const courseprogram = (app) => {
             if (!(await validateSession(req, res, response))) return;
             const controller = new ControllerCourseProgram();
             if (await controller.deleteCourseProgramByID(req.body.id) !== false) {
-                response.data = "El programa de curso fue elimonado correctamente";
+                response.data = "El programa de curso fue eliminado correctamente";
                 response.code = "200";
                 res.send(response);
             }
@@ -58,10 +58,8 @@ const courseprogram = (app) => {
         })
         .patch(async (req, res) => {
             if (!(await validateSession(req, res, response))) return;
-            const controller = new ControllerCourseProgram();
-            const result = await controller.updateCourseProgramByID(req.body.DSC_NAME,
-                req.body.DAT_YEAR, req.body.ID_STUDY_PLAN, req.body.NRC, req.body.CICLE,
-                req.body.NUM_CREDITS, req.body.SIGNATURE, req.session.usernameData[0].ID_USER, req.body.PDF_URL , req.body.ID_COURSE_PROGRAM ,req.body.STATE );
+            const controller = new ControllerCourseProgram();//, STATE, ID_COURSE_PROGRAM
+            const result = await controller.updateCourseProgramByID(req.body.DSC_NAME, req.body.DAT_YEAR, req.body.ID_STUDY_PLAN, req.body.NRC, req.body.CICLE, req.body.NUM_CREDITS, req.body.SIGNATURE, req.session.usernameData[0].ID_USER, req.body.PDF_URL, req.body.STATE, req.body.ID_COURSE_PROGRAM);
 
             if (result) {
                 response.data = "El programa de curso fue Actualizado correctamente";
